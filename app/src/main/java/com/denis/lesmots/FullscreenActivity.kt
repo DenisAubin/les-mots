d8200bcd998381ed9c3ee7fdeb026164a4d92afd
Denis Aubin <denis.aubin@mythalesgroup.io>
2022-02-17 09:53:55 +0100

    Initial commit, can write in table

---
+++ app/src/main/java/com/denis/lesmots/FullscreenActivity.kt
@@ -0,0 +1,240 @@
+package com.denis.lesmots
+
+import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
+import android.os.Build
+import android.os.Bundle
+import android.os.Handler
+import android.view.MotionEvent
+import android.view.View
+import android.view.WindowInsets
+import android.widget.*
+import androidx.core.view.get
+import com.denis.lesmots.databinding.ActivityFullscreenBinding
+
+/**
+ * An example full-screen activity that shows and hides the system UI (i.e.
+ * status bar and navigation/system bar) with user interaction.
+ */
+class FullscreenActivity : AppCompatActivity() {
+
+    private var rowPointer = 0
+    private var colPointer = 0
+    private lateinit var binding: ActivityFullscreenBinding
+    private lateinit var fullscreenContent: TextView
+    private lateinit var fullscreenContentControls: LinearLayout
+    private val hideHandler = Handler()
+    private var tab = arrayOf(CharArray(5),CharArray(5),CharArray(5),CharArray(5),CharArray(5),CharArray(5))
+    public var c = "Z"
+
+    @SuppressLint("InlinedApi")
+    private val hidePart2Runnable = Runnable {
+        // Delayed removal of status and navigation bar
+        if (Build.VERSION.SDK_INT >= 30) {
+            fullscreenContent.windowInsetsController?.hide(WindowInsets.Type.statusBars() or WindowInsets.Type.navigationBars())
+        } else {
+            // Note that some of these constants are new as of API 16 (Jelly Bean)
+            // and API 19 (KitKat). It is safe to use them, as they are inlined
+            // at compile-time and do nothing on earlier devices.
+            fullscreenContent.systemUiVisibility =
+                    View.SYSTEM_UI_FLAG_LOW_PROFILE or
+                            View.SYSTEM_UI_FLAG_FULLSCREEN or
+                            View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                            View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
+                            View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
+                            View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+        }
+    }
+    private val showPart2Runnable = Runnable {
+        // Delayed display of UI elements
+        supportActionBar?.show()
+        fullscreenContentControls.visibility = View.VISIBLE
+    }
+    private var isFullscreen: Boolean = false
+
+    private val hideRunnable = Runnable { hide() }
+
+    /**
+     * Touch listener to use for in-layout UI controls to delay hiding the
+     * system UI. This is to prevent the jarring behavior of controls going away
+     * while interacting with activity UI.
+     */
+    private val delayHideTouchListener = View.OnTouchListener { view, motionEvent ->
+        when (motionEvent.action) {
+            MotionEvent.ACTION_DOWN -> if (AUTO_HIDE) {
+                delayedHide(AUTO_HIDE_DELAY_MILLIS)
+            }
+            MotionEvent.ACTION_UP -> view.performClick()
+            else -> {
+            }
+        }
+        false
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityFullscreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        isFullscreen = true
+
+        // Set up the user interaction to manually show or hide the system UI.
+        fullscreenContent = binding.titleText
+        fullscreenContent.setOnClickListener { toggle() }
+
+        fullscreenContentControls = binding.linearLayout
+
+        // Upon interacting with UI controls, delay any scheduled hide()
+        // operations to prevent the jarring behavior of controls going away
+        // while interacting with the UI.
+        binding.buttona.setOnTouchListener(delayHideTouchListener)
+    }
+
+    override fun onPostCreate(savedInstanceState: Bundle?) {
+        super.onPostCreate(savedInstanceState)
+
+        // Trigger the initial hide() shortly after the activity has been
+        // created, to briefly hint to the user that UI controls
+        // are available.
+        delayedHide(100)
+    }
+
+    private fun toggle() {
+        if (isFullscreen) {
+            hide()
+        } else {
+            show()
+        }
+    }
+
+    private fun hide() {
+        // Hide UI first
+        supportActionBar?.hide()
+        fullscreenContentControls.visibility = View.GONE
+        isFullscreen = false
+
+        // Schedule a runnable to remove the status and navigation bar after a delay
+        hideHandler.removeCallbacks(showPart2Runnable)
+        hideHandler.postDelayed(hidePart2Runnable, UI_ANIMATION_DELAY.toLong())
+    }
+
+    private fun show() {
+        // Show the system bar
+        if (Build.VERSION.SDK_INT >= 30) {
+            fullscreenContent.windowInsetsController?.show(WindowInsets.Type.statusBars() or WindowInsets.Type.navigationBars())
+        } else {
+            fullscreenContent.systemUiVisibility =
+                    View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
+                            View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+        }
+        isFullscreen = true
+
+        // Schedule a runnable to display UI elements after a delay
+        hideHandler.removeCallbacks(hidePart2Runnable)
+        hideHandler.postDelayed(showPart2Runnable, UI_ANIMATION_DELAY.toLong())
+    }
+
+    /**
+     * Schedules a call to hide() in [delayMillis], canceling any
+     * previously scheduled calls.
+     */
+    private fun delayedHide(delayMillis: Int) {
+        hideHandler.removeCallbacks(hideRunnable)
+        hideHandler.postDelayed(hideRunnable, delayMillis.toLong())
+    }
+
+    companion object {
+        /**
+         * Whether or not the system UI should be auto-hidden after
+         * [AUTO_HIDE_DELAY_MILLIS] milliseconds.
+         */
+        private const val AUTO_HIDE = true
+
+        /**
+         * If [AUTO_HIDE] is set, the number of milliseconds to wait after
+         * user interaction before hiding the system UI.
+         */
+        private const val AUTO_HIDE_DELAY_MILLIS = 3000
+
+        /**
+         * Some older devices needs a small delay between UI widget updates
+         * and a change of the status and navigation bar.
+         */
+        private const val UI_ANIMATION_DELAY = 300
+    }
+
+    fun getActiveTile() : TextView {
+        val row= binding.wordTable[rowPointer] as TableRow
+        return row[colPointer] as TextView
+    }
+
+    fun getPreviousTile() : TextView {
+        if (rowPointer ==0 && colPointer==0){
+            return getActiveTile()
+        }
+        if(colPointer==0){
+            val row= binding.wordTable[rowPointer-1] as TableRow
+            return row[4] as TextView
+        }
+        val row= binding.wordTable[rowPointer] as TableRow
+        return row[colPointer-1] as TextView
+    }
+
+    fun getNextTile() : TextView {
+        if (rowPointer ==5 && colPointer==4){
+            return getActiveTile()
+        }
+        if(colPointer==4){
+            val row= binding.wordTable[rowPointer+1] as TableRow
+            return row[0] as TextView
+        }
+        val row= binding.wordTable[rowPointer] as TableRow
+        return row[colPointer+1] as TextView
+    }
+
+    fun onKeyClick(view: View) {
+        if(colPointer<5){
+            val b= view as Button
+            val case = getActiveTile()
+            case.text=b.text;
+            colPointer++
+            onSelectedTileChange()
+        }
+    }
+
+    fun onEnterClick(view: View){
+       if(rowPointer<5) {
+           rowPointer++
+           colPointer=0
+           onSelectedTileChange()
+       }
+    }
+
+    fun onReturnClick(view: View){
+        if(colPointer>0){
+            colPointer--
+            val case = getActiveTile()
+            case.text="";
+            onSelectedTileChange(false)
+        }
+    }
+
+    private fun onSelectedTileChange(b:Boolean=true){
+        if(b){
+            if(colPointer<5 && rowPointer<6){
+                getActiveTile().background= resources.getDrawable(R.drawable.active_char_background,theme)
+            }
+            getPreviousTile().background= resources.getDrawable(R.drawable.char_background,theme)
+        }else{
+            if(colPointer<5 && rowPointer<6){
+                getNextTile().background= resources.getDrawable(R.drawable.char_background,theme)
+            }
+            getActiveTile().background= resources.getDrawable(R.drawable.active_char_background,theme)
+        }
+
+    }
+}
